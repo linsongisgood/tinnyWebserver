e3c18bb9318813c43d3de254f21c0c4830b3efe2
rxq <2839298066@qq.com>
2024-09-07 12:57:59 +0800

    将生成的日志文件放到同一个目录下

--- webserver.cpp
+++ webserver.cpp
@@ -78,9 +78,9 @@ void WebServer::log_write()
     {
         //初始化日志
         if (1 == m_log_write)
-            Log::get_instance()->init("./ServerLog", m_close_log, 2000, 800000, 800); //异步写入日志
+            Log::get_instance()->init("./log_record/ServerLog", m_close_log, 2000, 800000, 800); //异步写入日志
         else
-            Log::get_instance()->init("./ServerLog", m_close_log, 2000, 800000, 0);  //同步写入日志
+            Log::get_instance()->init("./log_record/ServerLog", m_close_log, 2000, 800000, 0);  //同步写入日志
     }
 }
 
